f0a5fa21ffe2a1f171bd37960378b0bb1931009d
Francesco Maurella <francesco.maurella@live.it>
2021-02-09 16:59:12 +0100

    Creazione iniziale stili + funzione Search (con axios) attiva

--- js/script.js
+++ js/script.js
@@ -4,11 +4,28 @@ new Vue ({
 
   // DATI
   data : {
+    movies : [],
+    searchKey : ''
 
   },
-  
+
   // METODI
   methods : {
+    // Funzione cerca movie
+    movieSearch : function(){
+      const self = this;
 
-  },
-});
+      axios.get(
+        'https://api.themoviedb.org/3/search/movie?api_key=a7cdee9ef91ea5bb1fa70822158b4b0b&query='
+        + this.searchKey)
+        .then(function(resp){
+          // creazione lista movies
+          self.movies = resp.data.results
+        });
+      },
+      // Funzione recupero anno movie
+      getMovieYear : function(data) {
+      return data.slice(0, 4)
+      }
+    }
+  });
