b0a2b3136ccc575fe9a04187d6c4dd9b01aa32a9
Francesco Maurella <francesco.maurella@live.it>
2021-02-10 13:53:11 +0100

    Stelle e bandiere

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ new Vue ({
       const self = this;
 
       axios.get(
-        'https://api.themoviedb.org/3/search/movie?api_key=a7cdee9ef91ea5bb1fa70822158b4b0b&query='
-        + this.searchKey)
+        'https://api.themoviedb.org/3/search/movie?api_key=a7cdee9ef91ea5bb1fa70822158b4b0b&query=titanic')
+        // + this.searchKey)
         .then(function(resp){
           // creazione lista movies
           self.movies = resp.data.results
@@ -25,7 +25,20 @@ new Vue ({
       },
       // Funzione recupero anno movie
       getMovieYear : function(data) {
-      return data.slice(0, 4)
-      }
+        return data.slice(0, 4)
+      },
+      // Funzione voto
+      getVote : function(vote, index) {
+        const num = Math.ceil(vote / 2);
+        const starClasses = []
+        for (var i = 0; i < 5; i++) {
+          if (i < num) {
+            starClasses.push('fas')
+          } else {
+            starClasses.push('far')
+          }
+        }
+        return starClasses[index-1];
+      },
     }
   });
