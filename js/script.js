39d5611013ac8c3c383f578ac2a1fa33ce5f9291
Francesco Maurella <francesco.maurella@live.it>
2021-02-15 05:01:57 +0100

    Filtri, attori, generi, redesign

--- js/script.js
+++ js/script.js
@@ -4,45 +4,93 @@ new Vue ({
 
   // DATI
   data : {
+    apiUrl : 'https://api.themoviedb.org/3/',
+    apiKey : 'a7cdee9ef91ea5bb1fa70822158b4b0b',
     results : [],
-    query : ''
+    genres : [],
+    cast : '',
+    query : '',
+    selected : ''
   },
 
   // METODI
   methods : {
     // Funzione richiamo TMDB Api
     getApi : function(type){
-      const searchIn = type;
       axios.get(
-        'https://api.themoviedb.org/3/search/' + type , {
+        this.apiUrl + 'search/' + type , {
           params : {
-            api_key : 'a7cdee9ef91ea5bb1fa70822158b4b0b',
+            api_key : this.apiKey,
             query : this.query,
             language : 'it-IT'
           }
         }).then((resp) => {
           this.results = [...this.results, ...resp.data.results]
         });
-      },
-      // Funzione ricerca
-      getSearch : function(data){
-        this.results = [];
-        this.getApi('movie');
-        this.getApi('tv');
-      },
-      // Funzione voto
-      getVote : function(vote, index) {
-        const num = Math.ceil(vote / 2);
-        const starClasses = [];
-        for (var i = 0; i < 5; i++) {
-          if (i < num) {
-            starClasses.push('fas')
-          } else {
-            starClasses.push('far')
-          }
-        }
-        return starClasses[index-1];
-      },
-    }
+        axios.get(
+          this.apiUrl + 'genre/' + type + '/list', {
+            params : {
+              api_key : this.apiKey,
+            }
+          }).then((resp) => {
+            resp.data.genres.forEach((element) => {
+              if (!this.genres.includes(element)) {
+                this.genres.push(element)
+              }
+            });
+          });
+        },
+        // Funzione ricerca
+        getSearch : function(data){
+          this.results = [];
+          this.genres = [];
+          this.getApi('movie');
+          this.getApi('tv');
+        },
+        // Funzione Visualizza Se
+        showIf : function(item){
+          let id;
+          this.genres.forEach((element) => {
+            if (element.name === this.selected) {
+              console.log(element.id)
+              id = element.id
+            }
+          })
 
-  });
+          return item['genre_ids'].includes(id) || this.selected === ''
+        },
+        // Funzione Cast
+        showCast : function(id){
+          const cast = [];
+          axios.get(
+            this.apiUrl + 'movie/' + id + '/credits', {
+              params : {
+                api_key : this.apiKey,
+                language : 'en-US'
+              }
+            }).then((resp) => {
+              resp.data.cast.forEach((element) => {
+                cast.push(element.name)
+              });
+              this.cast = cast.splice(0, 5).join(', ')
+            });
+          },
+          // Funzione generi
+          inGenres : function(array, element){
+            return array.genre_ids.includes(element.id)
+          },
+          // Funzione voto
+          getVote : function(vote, index) {
+            const num = Math.ceil(vote / 2);
+            const starClasses = [];
+            for (var i = 0; i < 5; i++) {
+              if (i < num) {
+                starClasses.push('fas')
+              } else {
+                starClasses.push('far')
+              }
+            }
+            return starClasses[index-1];
+          },
+        }
+      });
